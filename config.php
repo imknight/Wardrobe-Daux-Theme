b56386679bdda9c0f3be43025c0cc876d9861bcf
Knight Tan <knight.ashodin@gmail.com>
2013-08-12 20:35:52 +0800

    added more option to config file

--- config.php
+++ config.php
@@ -2,5 +2,15 @@
 return array(
 	'tagline' => 'The Easiest Way To Document Your Project',
 	'theme' => 'navy',
-  	'copyright' => '&copy; Your Name',
+	'date_modified' => true,
+	'repo' => 'justinwalsh/daux.io',
+	'twitter' => array( 'justin_walsh', 'todaymade', 'imknight' ),
+	'google_analytics' => false,
+	'piwik_analytics' => false,
+	'links' =>array(
+		'Download' => '',
+		'GitHub Repo' => '',
+		'Help/Support/Bugs' => '',
+		'Made by Todaymade' => 'http://todaymade.com'
+	),
 );
